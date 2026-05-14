5d188a6a527007ddc08a3aef39b7fea6567d1282
apomerenk <alex.pomerenk@gmail.com>
2026-05-14 12:39:57 -0400

    Bump github-notifications-menu-bar to v1.0.0

--- Casks/github-notifications-menu-bar.rb
+++ Casks/github-notifications-menu-bar.rb
@@ -1,6 +1,6 @@
 cask "github-notifications-menu-bar" do
-  version "0.0.0"
-  sha256 "0000000000000000000000000000000000000000000000000000000000000000"
+  version "1.0.0"
+  sha256 "d5cf9754061eec091661c5009f750a9847bec8436d32ff7ae9e11d179c216be0"
 
   url "https://github.com/apomerenk/github-notification-menu-bar/releases/download/v#{version}/GitHubNotifications-#{version}.zip"
   name "GitHub Notifications"
